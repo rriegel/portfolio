a76c99712f59b950ad6ccf91793418512aa4f95e
rriegel <ryan.j.riegel@gmail.com>
2024-10-23 12:59:10 -0400

    changed imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Button, ButtonGroup } from '@mui/material';
 import Home from './routes/Home';
 import Projects from './routes/Projects';
