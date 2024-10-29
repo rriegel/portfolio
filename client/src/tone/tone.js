c34d109c6dfb42c67682c7493f326976fe8e4572
rriegel <ryan.j.riegel@gmail.com>
2024-10-29 09:36:58 -0400

    adding fx to synth

--- client/src/tone/tone.js
+++ client/src/tone/tone.js
@@ -1,12 +1,39 @@
 import * as Tone from "tone";
 
 let synth;
+let chorus;
+let freeverb;
+let phaser;
+let tremelo;
 
 async function onStart() {
   await Tone.start();
 
-  synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  // synth = new Tone.PolySynth(Tone.Synth).toDestination();
+
+  chorus = new Tone.Chorus().toDestination();
+  freeverb = new Tone.Freeverb().toDestination();
+  // synth = new Tone.PolySynth(Tone.Synth).connect(chorus);
+  freeverb.dampening = 1000;
+  synth = new Tone.PolySynth(Tone.Synth).connect(chorus);
 };
+
+function turnOnChorus() {
+  chorus.start();
+}
+
+function turnOffChorus() {
+  chorus.stop();
+  synth.disconnect(chorus);
+}
+
+function turnOnFreeverb() {
+  synth.connect(freeverb);
+}
+
+function turnOffFreeverb() {
+  synth.disconnect(freeverb);
+}
 
 function addNote(note) {
   if (!synth) return;
@@ -18,7 +45,7 @@ function addNote(note) {
 
 function removeNote(note) {
   if (!synth) return;
-  
+
   const now = Tone.now();
   let octave = "4";
   synth.triggerRelease(note+octave, now);
